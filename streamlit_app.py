5a2cc4bc721a166794e9f0ba906832bbc685e218
anandmaurya07 <anandmaurya1532@gmail.com>
2024-10-19 16:23:48 +0530

    rag_granth_v1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,53 +1,116 @@
 import streamlit as st
 from openai import OpenAI
 
-# Show title and description.
-st.title("📄 Document question answering")
-st.write(
-    "Upload a document below and ask a question about it – GPT will answer! "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-)
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
+# # # # # # #
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+os.environ['OPENAI_API_KEY']=os.getenv("OPENAI_API_KEY")
 
-    # Let the user upload a file via `st.file_uploader`.
-    uploaded_file = st.file_uploader(
-        "Upload a document (.txt or .md)", type=("txt", "md")
+def loading_pdf_and_embedding(file_path,db_path):
+    #Text extraction form pdf
+    from PyPDF2 import PdfReader    
+
+    ##file_path = save_path+"/granth-rag.pdf"
+    reader = PdfReader(file_path)
+    text = ""
+    #for page in reader.pages:
+    #    text += page.extract_text()    
+
+    start_page = 13  
+    end_page = 18   
+
+    for page_num in range(start_page, end_page + 1):
+        page = reader.pages[page_num]
+        text += page.extract_text() 
+
+    #print(text)    
+
+    #chunking
+    from langchain_text_splitters import RecursiveCharacterTextSplitter 
+
+    text_splitter=RecursiveCharacterTextSplitter(chunk_size=800, chunk_overlap = 50)
+    chunks=text_splitter.split_text(text)
+    #chunks 
+
+    #embed and store
+    from langchain.vectorstores import Chroma
+    from langchain.embeddings import OpenAIEmbeddings   
+
+    embedding = OpenAIEmbeddings(model="text-embedding-3-large")
+    vectordb= Chroma.from_texts(texts=chunks,embedding=embedding,persist_directory=db_path)  
+
+
+
+def get_answer(query):
+    from langchain.vectorstores import Chroma # needed when we run this function in a separate cell
+    from langchain.embeddings import OpenAIEmbeddings   
+    # Load the persisted vector store
+    vectordbt = Chroma(persist_directory=db_path, embedding_function=OpenAIEmbeddings(model="text-embedding-3-large"))
+    
+
+    docs = vectordbt.similarity_search(query)
+
+    from langchain.prompts import PromptTemplate
+    prompt_template = PromptTemplate(
+        input_variables = ['query','context'],
+        template="""You are an expert assistant for question-answering.
+        Here in context religious document will be provided to you it can be any religious holybook,story , prayer ,research paper.
+        your job is to read , understand and analyze the context before answering the question.
+        user will ask u questions about the context and you have to answer them.
+        feel free to answer the question briefly if needed, just Keep the answer human friendly,clear and to the point.
+
+        Note : If you don't know the answer, just say that you don't know. Don't make assumptions.
+
+        Given the context below,answer the question:\n\n
+        Context: {context}\n\n
+        Question: {question}\n\n
+        Answer:
+    """
     )
+    
+    from langchain.chains import LLMChain
+    from langchain.chat_models import ChatOpenAI
+    llm = ChatOpenAI(model='gpt-4o-mini',temperature=0,max_tokens=200)#
+    chain = LLMChain(llm=llm, prompt=prompt_template)
 
-    # Ask the user for a question via `st.text_area`.
-    question = st.text_area(
-        "Now ask a question about the document!",
-        placeholder="Can you give me a short summary?",
-        disabled=not uploaded_file,
-    )
+    return chain.run(question=query, context=docs)
 
-    if uploaded_file and question:
+# # # # # # #
 
-        # Process the uploaded file and question.
-        document = uploaded_file.read().decode()
-        messages = [
-            {
-                "role": "user",
-                "content": f"Here's a document: {document} \n\n---\n\n {question}",
-            }
-        ]
 
-        # Generate an answer using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=messages,
-            stream=True,
-        )
+import streamlit as st
+from openai import OpenAI
 
-        # Stream the response to the app using `st.write_stream`.
-        st.write_stream(stream)
+# Title
+st.markdown("<h1 style='text-align: center;'>GRANTH-RAG</h1>", unsafe_allow_html=True)
+st.markdown("<h6 style='text-align: center;'>Here you can question-answer on your religious 📄 document</h6>", unsafe_allow_html=True)
+
+uploaded_file = st.file_uploader("Upload your document here (.pdf)", type="pdf")
+
+save_path = "/Users/anandmaurya/Desktop/Cilans/PDF_RAG/granth-rag/File"
+specific_filename = "granth-rag.pdf"
+import os
+
+if uploaded_file:
+    # Save the uploaded file to the specified path
+    file_path = os.path.join(save_path, specific_filename)
+    with open(file_path, "wb") as f:
+        f.write(uploaded_file.getbuffer())
+    
+    
+    file_path = '/Users/anandmaurya/Desktop/Cilans/PDF_RAG/granth-rag/File/granth-rag.pdf'
+    db_path = '/Users/anandmaurya/Desktop/Cilans/PDF_RAG/granth-rag/chroma_db'
+    
+    loading_pdf_and_embedding(file_path,db_path)
+
+question = st.text_input("Ask a question")
+
+if st.button("Submit"):
+    if question:
+        answer = get_answer(question)
+        st.write(answer)
+    else:
+        st.write("Please upload a PDF and enter a question.")
